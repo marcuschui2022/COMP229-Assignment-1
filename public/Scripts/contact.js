7c55d77e33ce976eb1e06e31f080d673e0bc23a0
Marcus Chui <marcus.chui.ca@icloud.com>
2023-02-04 23:11:17 -0500

    fix contact.js

--- public/Scripts/contact.js
+++ public/Scripts/contact.js
@@ -3,14 +3,13 @@
 // Last Modified Date: 4 Feb 2023
 
 const form = document.getElementById("contactForm");
-const formData = new FormData(form);
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   let displayMsg = "This is the details you want to send: \n";
-
-  // console.log(formData);
+  const formData = new FormData(form);
+  console.log(formData);
   for (const [key, value] of formData) {
     // console.log(displayMsg);
     displayMsg += `${key}: ${value}\n`;
