4c997d3e252bf5065c2c8502db4065f6be276275
Marcus Chui <marcus.chui.ca@icloud.com>
2023-02-15 22:28:49 -0500

    just update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 // console.log(process.env.MongoConnectionString);
 module.exports = {
-  URI: process.env.MongoConnectionString,
+  URI: "mongodb+srv://marcus:yuk60677632@cluster0.hvkjusf.mongodb.net/comp229-assignment?retryWrites=true&w=majority",
 };
